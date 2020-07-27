b96285ec71c169c24775f2e62f8ac99fae626c74
SanaAbuAliSheikh <sanaasheikh99@gmail.com>
2020-07-28 04:39:58 +0500

    mernapp

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const app = express();
-
+const path = require("path");
 connectDB();
 
 const PORT = process.env.PORT || 5000;
@@ -21,6 +21,9 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/user", require("./routes/api/task"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
 }
 app.listen(PORT, () => {
   console.log(`Server Started on Port ${PORT}`);
